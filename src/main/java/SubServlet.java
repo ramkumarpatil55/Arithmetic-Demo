c0f3532e261ea4c58e3e1cd6964151dc51d7a768
ramkumarpatil55 <107768378+ramkumarpatil55@users.noreply.github.com>
2022-09-23 01:22:08 +0530

    initial commit

---
+++ src/main/java/SubServlet.java
@@ -0,0 +1,27 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class SubServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+PrintWriter pw=response.getWriter();
+		
+		int x=Integer.parseInt(request.getParameter("n1"));
+		int y=Integer.parseInt(request.getParameter("n2"));
+		
+		int z=x-y;
+		
+		pw.write("<h1>Sub is "+z+"</h1>");
+		pw.write("<a href='index.html'>Back to Menu.....</a>");
+	}
+}
